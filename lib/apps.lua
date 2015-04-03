bd56594d36f7b33540998e7b8abb1b9df4e8d722
Rob Hilgefort <rjhilgefort@gmail.com>
2015-04-03 19:05:14 -0400

    Added some apps, then broke the toLaunch and toKill
    (WIP) I'm setting up an application directory, so to speak

--- lib/apps.lua
+++ lib/apps.lua
@@ -1,28 +1,64 @@
-apps = {}
-
-apps.toLaunch = {
-    'iTerm', 'Google Chromee', 'Finder', 'Sonos', 'Messages', 'Spotify',
-    'Simplify', '1Password mini', 'Clear', 'Sunrise', 'Airmail Beta',
-    'Dash', 'Blink1Control'
+-- List of all apps
+apps = {
+    'iTerm',
+    chrome = 'Google Chrome',
+    'Finder',
+    'Sonos',
+    'Messages',
+    'Spotify',
+    'Simplify',
+    onePassword = '1Password',
+    onePasswordMini = '1Password mini',
+    'Clear',
+    'Sunrise',
+    airmail = { app = 'Airmail Beta', title = 'Airmail' },
+    'Dash',
+    blink = 'Blink1Control',
+    'Hammerspoon',
+    'HipChat',
+    'Dropbox',
+    'iTunes',
+    'System Preferences',
+    battleNet = 'Battle.net',
+    'Xcode',
+    'Cobook'
 }
-apps.toKill = {
-    'iTunes',   'System Preferences', '1Password', 'Battle.net'
+
+-- process apps
+print("\n\n\nAPPS DEF:")
+for _,app in pairs(apps) do
+    print(app)
+end
+print("\n\n\n")
+
+
+toLaunch = {
+    'iTerm', 'Google Chrome', 'Finder', 'Sonos', 'Messages', 'Spotify',
+    'Simplify', '1Password mini', 'Clear', 'Sunrise', 'Airmail Beta',
+    'Dash', 'Blink1Control', 'Hammerspoon', 'HipChat', 'Dropbox'
+}
+toKill = {
+    'iTunes',   'System Preferences', '1Password', 'Battle.net', 'Xcode',
+    'Cobook'
 }
 
 -- TODO:
 -- Capture all visible windows on all screens before doing anything else, then
 --      refocus them after launching all the things
 function apps.launchApps()
-    for i,app in pairs(apps.toLaunch) do
-        hs.application.launchOrFocus(app)
-        app = hs.appfinder.appFromName(app)
-        if (app ~= nil) then app:hide() end
+    for _,appName in pairs(toLaunch) do
+        hs.application.launchOrFocus(appName)
+        local app = hs.appfinder.appFromName(appName)
+        if (app ~= nil) then
+            print("Hiding " .. appName)
+            app:hide()
+        end
     end
 end
 
 -- Kill any running apps in the kill list
 function apps.killApps()
     for i,app in pairs(hs.application.runningApplications()) do
-        if (hs.fnutils.contains(apps.toKill, app:title())) then app:kill() end
+        if (hs.fnutils.contains(toKill, app:title())) then app:kill() end
     end
 end
